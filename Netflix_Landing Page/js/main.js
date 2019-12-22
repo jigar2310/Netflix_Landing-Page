d2df9d0a76ee18698a422a482168751328cf7ab8
anilprajapati1006 <51775205+anilprajapati1006@users.noreply.github.com>
2019-12-23 00:36:51 +0530

    Netflix Landing Page using HTML, CSS & Javascript
    https://edvinenetflix.netlify.com/

---
+++ Netflix_Landing Page/js/main.js
@@ -0,0 +1,29 @@
+const tabItems = document.querySelectorAll('.tab-item');
+
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+//Select Tab Content Item 
+function selectItem(e) {
+    removeBorder();
+    removeShow();
+    //Add border to current tab
+    this.classList.add('tab-border');
+    //Grab content item from DOM 
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // console.log(this.id+'-content');
+    
+    //Add show class
+    tabContentItem.classList.add('show');
+
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+//Listen fot tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
